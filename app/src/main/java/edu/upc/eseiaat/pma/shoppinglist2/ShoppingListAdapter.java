76e37ae527343d156f63aa81afcb38041fbbff2e
Marc Montserrat <marcmontserrrat@gmail.com>
2017-10-23 13:27:01 +0200

    Initial commit

---
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist2/ShoppingListAdapter.java
@@ -0,0 +1,11 @@
+package edu.upc.eseiaat.pma.shoppinglist2;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by marc.montserrat.robl on 23/10/2017.
+ */
+
+public class ShoppingListAdapter extends ArrayAdapter {
+
+}
